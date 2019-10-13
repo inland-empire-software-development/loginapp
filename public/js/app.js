27c28e5b6f336265734f01aef78746c6e0c3e5b3
David <daveshuang@gmail.com>
2019-10-13 15:15:33 -0700

    renamed form button to check in and centered/ added basic crollbar to autocomplete

--- public/js/app.js
+++ public/js/app.js
@@ -46,39 +46,53 @@ $.get("/userlist", function (data) {
         /*execute a function when someone writes in the text field:*/
         inp.addEventListener("input", function (e) {
             var a, b, i, val = this.value;
+
             /*close any already open lists of autocompleted values*/
             closeAllLists();
+
             if (!val) { return false; }
             currentFocus = -1;
+
             /*create a DIV element that will contain the items (values):*/
             a = document.createElement("DIV");
             a.setAttribute("id", this.id + "autocomplete-list");
             a.setAttribute("class", "autocomplete-items");
+
             /*append the DIV element as a child of the autocomplete container:*/
             this.parentNode.appendChild(a);
+
             /*for each item in the array...*/
             for (i = 0; i < arr.length; i++) {
+
                 /*check if the item starts with the same letters as the text field value:*/
                 if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
                     /*create a DIV element for each matching element:*/
                     b = document.createElement("DIV");
+
                     /*make the matching letters bold:*/
                     b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
                     b.innerHTML += arr[i].substr(val.length);
+
                     /*insert a input field that will hold the current array item's value:*/
                     b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
                     /*execute a function when someone clicks on the item value (DIV element):*/
                     b.addEventListener("click", function (e) {
+
                         /*insert the value for the autocomplete text field:*/
                         inp.value = this.getElementsByTagName("input")[0].value;
+
                         /*close the list of autocompleted values,
                         (or any other open lists of autocompleted values:*/
                         closeAllLists();
                     });
+
                     a.appendChild(b);
                 }
             }
         });
+
         /*execute a function presses a key on the keyboard:*/
         inp.addEventListener("keydown", function (e) {
             var x = document.getElementById(this.id + "autocomplete-list");
@@ -104,6 +118,7 @@ $.get("/userlist", function (data) {
                 }
             }
         });
+
         function addActive(x) {
             /*a function to classify an item as "active":*/
             if (!x) return false;
@@ -114,12 +129,14 @@ $.get("/userlist", function (data) {
             /*add class "autocomplete-active":*/
             x[currentFocus].classList.add("autocomplete-active");
         }
+
         function removeActive(x) {
             /*a function to remove the "active" class from all autocomplete items:*/
             for (var i = 0; i < x.length; i++) {
                 x[i].classList.remove("autocomplete-active");
             }
         }
+
         function closeAllLists(elmnt) {
             /*close all autocomplete lists in the document,
             except the one passed as an argument:*/
@@ -130,11 +147,13 @@ $.get("/userlist", function (data) {
                 }
             }
         }
+
         /*execute a function when someone clicks in the document:*/
         document.addEventListener("click", function (e) {
             closeAllLists(e.target);
         });
     }
+    
     autocomplete(document.getElementById("myInput"), users);
 
 
