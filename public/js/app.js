5c5094bf191eae94e76ef02ec0f9a0b798009a30
Andy <andymendez100@gmail.com>
2019-10-13 01:44:45 -0700

    Finished back end for api through meetup. Got users to populated to database as well

--- public/js/app.js
+++ public/js/app.js
@@ -7,10 +7,6 @@ const slide4 = document.querySelector("#slide4");
 const slide5 = document.querySelector("#slide5");
 
 const tl = new TimelineMax();
-// let d = new Date();
-// let n = d.getDate();
-
-
 
 //------------------------
 //   top-down animation
@@ -26,39 +22,22 @@ tl.fromTo(front, 1, { height: "0%" }, { height: "65%", ease: Power2.easeInOut })
     .fromTo(slide5, 1.2, { y: "-105%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
 
 
-// Js for checking if today's code is correct
+// Creating a variable to hold all the users
+
 let users;
 
-
+// Route to get all the user
 $.get("/userlist", function (data) {
-    // console.log(data);
-
-    // for (let j = 0; j < data.length; j++) {
     users = data
 
-    // }
-    // console.log(data);
-    // code = data;
     const errorPopup = document.getElementById("errorPopup");
     const loader = document.getElementById("loader");
     const codeSubmit = document.getElementById("codeSubmit");
 
-    $(".checkIn").on('submit', (event) => {
-        event.preventDefault();
-        var userCode = document.getElementById("myForm").elements[0].value;
-        userCode = parseInt(userCode);
-        if (code === userCode) {
-            errorPopup.style.display = "none";
-            loader.style.display = "block";
-            codeSubmit.style.display = "none";
-            window.location = "https://github.com/login/oauth/authorize?client_id=dd87eb0d1732bf5e0f5b&scope=user&redirect_uri=http://localhost:8080/oauth/redirect";
-        }
-        else {
-            errorPopup.style.display = "flex";
-        }
-    })
+    // $(".checkIn").on('submit', (event) => {
+    //     event.preventDefault();
 
-    // var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
+    // })
 
     function autocomplete(inp, arr) {
         /*the autocomplete function takes two arguments,
@@ -160,53 +139,3 @@ $.get("/userlist", function (data) {
 
 
 })
-
-
-
-
-// //------------------------
-// //   steps-up animation
-// //------------------------
-// tl.fromTo(front, 1, { height: "0%" }, { height: "65%", ease: Power2.easeInOut })
-//     .fromTo(front, 1.2, { width: "50%" }, { width: "26%", ease: Power2.easeInOut })
-//     .fromTo(slide1, 1.2, { y: "100%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide2, 1.2, { y: "125%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide3, 1.2, { y: "150%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide4, 1.2, { y: "175%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide5, 1.2, { y: "200%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-
-// //------------------------
-// //   steps-down animation
-// //------------------------
-// tl.fromTo(front, 1, { height: "0%" }, { height: "65%", ease: Power2.easeInOut })
-//     .fromTo(front, 1.2, { width: "50%" }, { width: "26%", ease: Power2.easeInOut })
-//     .fromTo(slide1, 1.2, { y: "-100%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide2, 1.2, { y: "-125%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide3, 1.2, { y: "-150%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide4, 1.2, { y: "-175%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide5, 1.2, { y: "-200%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-
-
-// //------------------------
-// //   left-side animation
-// //------------------------
-// tl.fromTo(front, 1, { height: "0%" }, { height: "65%", ease: Power2.easeInOut })
-//     .fromTo(front, 1.2, { width: "50%" }, { width: "26%", ease: Power2.easeInOut })
-//     .fromTo(slide1, 1.2, { x: "-105%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide2, 1.2, { x: "-205%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide3, 1.2, { x: "-305%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide4, 1.2, { x: "-405%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide5, 1.2, { x: "-505%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-
-
-//------------------------
-//   custom animation
-//------------------------
-// tl.fromTo(front, 1, { height: "0%" }, { height: "65%", ease: Power2.easeInOut })
-//     .fromTo(front, 1.2, { width: "50%" }, { width: "26%", ease: Power2.easeInOut })
-//     .fromTo(slide1, 1.2, { x: "-105%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide2, 1.2, { x: "-205%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide3, 1.2, { y: "-100%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide4, 1.2, { x: "205%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-//     .fromTo(slide5, 1.2, { x: "105%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-
--- routes/auth-github.js
+++ routes/auth-github.js
@@ -1,52 +1,19 @@
-const path = require('path');
 const axios = require('axios');
-const keys = require('../config/keys');
-const mongoose = require('mongoose');
 const User = require('../models/User');
 const Event = require('../models/Event');
 
-// console.log(new Date(new Date().getFullYear(),new Date().getMonth() , new Date().getDate()));
 
-/**
- * This is the client ID and client secret provided by GitHub
- * when you register for an OAuth app.
- */
-const clientID = keys.clientID;
-const clientSecret = keys.clientSecret;
+
 const eventDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
-let code = Math.floor(100000 + Math.random() * 900000)
 
-// const createUserDb = (username, email) => {
-//     const user = new User({
-//         username,
-//         email
-//     })
-//     user.save()
-//         .then(result => {
-//             console.log(result);
-//         })
-//         .catch(err => console.log(err));
-// }
 
-//Add Event date each user that signs in 
-// const createEventDb = () => {
-// const eventDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
-//     const event = new Event({
-//         date: eventDate
-//     });
-//     event.save()
-//         .then(result => {
-//             console.log(result._id);
-//             Event.findOneAndUpdate({ date: eventDate }, { $push: { users: result._id } }, { new: true });
-//         })
-//         .catch(err => console.log(err));
-// }
-// Event.create({ date: eventDate })
-//     .then(function (dbDate) {
-//         console.log(dbDate);
-//     }).catch(function (err) {
-//         console.log(err.message);
-//     });
+
+Event.create({ date: eventDate })
+    .then(function (dbDate) {
+        console.log(dbDate);
+    }).catch(function (err) {
+        console.log(err.message);
+    });
 
 
 module.exports = app => {
@@ -109,131 +76,59 @@ module.exports = app => {
 
             for (let j = 0; j < users.length; j++) {
                 userMap.push(users[j].Name)
-
             }
-
-            // users.forEach(function (user) {
-            //     userMap[user._id] = user.Name;
-            // });
 
             res.send(userMap);
         });
     })
 
-    /**
-     * Route that GitHub redirects the user to
-     * once they give access to the application.
-     */
-    //     app.get('/oauth/redirect', (req, res) => {
-    //         const requestToken = req.query.code;
 
-    //         /**
-    //          * Post request to GitHub OAuth api with the client ID, 
-    //          * client secret and request token. Request token is given
-    //          * after user allows application access to their info.
-    //          */
-    //         axios({
-    //             method: 'post',
-    //             url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-    //             headers: {
-    //                 accept: 'application/json'
-    //             }
-    //         }).then((response) => {
+    app.post('/login', (req, res) => {
+        console.log(req.body.myMeetUp);
 
-    //             /** 
-    //              * Once we get the response, extract the access token from
-    //              * the response body
-    //              */
-    //             const accessToken = response.data.access_token;
-    //             axios({
-    //                 method: 'get',
-    //                 url: 'https://api.github.com/user/emails',
-    //                 headers: {
-    //                     'Authorization': "bearer " + accessToken
-    //                 }
-    //             })
-    //                 .then(response => {
-    //                     const data = response.data;
+        const userLogin = req.body.myMeetUp;
+        User.findOne({
+            Name: userLogin
+        })
+            .then((data) => {
+                // if (data) return Event.findOneAndUpdate({ date: eventDate }, { $push: { users: data._id } }, { new: true });
+                if (data) {
+                    Event.findOne({ date: eventDate, users: data._id })
+                        .then(user => {
+                            if (!user) return Event.findOneAndUpdate({ date: eventDate }, { $push: { users: data._id } }, { new: true });
+                        })
+                }
 
-    //                     /**
-    //                      * Loop through response and checks to see if 
-    //                      * the data provided is the primary information of 
-    //                      * the user.
-    //                      */
-    //                     data.forEach((item) => {
-    //                         if (item.primary === true) {
-    //                             axios({
-    //                                 method: 'get',
-    //                                 url: "https://api.github.com/user",
-    //                                 headers: {
-    //                                     'Authorization': "bearer " + accessToken
-    //                                 }
-    //                             })
-    //                                 .then(resp => {
-    //                                     User.findOne({
-    //                                         username: resp.data.login
-    //                                     }).then((data) => {
-    //                                         // if (data) return Event.findOneAndUpdate({ date: eventDate }, { $push: { users: data._id } }, { new: true });
-    //                                         if(data){
-    //                                             Event.findOne({ date: eventDate, users: data._id })
-    //                                             .then(user => {
-    //                                                 if(!user) return Event.findOneAndUpdate({ date: eventDate }, { $push: { users: data._id } }, { new: true });
-    //                                             })
-    //                                         }
+                return User.create({
+                    Name: userLogin,
 
-    //                                         return User.create({
-    //                                             username: resp.data.login,
-    //                                             email: item.email
-    //                                         }).then(function (dbUser) {
-    //                                             console.log(dbUser.email);
+                }).then(function (dbUser) {
 
-    //                                             return Event.findOneAndUpdate({ date: eventDate }, { $push: { users: dbUser._id } }, { new: true })
-    //                                         }).then(function (res) {
-    //                                             console.log(res);
+                    return Event.findOneAndUpdate({ date: eventDate }, { $push: { users: dbUser._id } }, { new: true })
+                }).then(function (res) {
+                    console.log(res);
 
-    //                                         }).catch(function (err) {
-    //                                             console.log(err.message);
-    //                                         });
+                }).catch(function (err) {
+                    console.log(err.message);
+                });
 
-    //                                     })
-    //                                     // createUserDb(resp.data.logn, item.email);
-    //                                     // createEventDb();
-    //                                     // console.log(resp.data);
-    //                                     res.redirect('/signedin')
-    //                                 })
-    //                         }
-    //                     });
-    //                 });
-    //         });
-    //     });
+            })
+        res.redirect('/signedin')
+    })
 
-    //     app.get("/populateduser", function (req, res) {
-    //         // Find all users
-    //         Event.find({})
-    //             // Specify that we want to populate the retrieved users with any associated notes
-    //             .populate("users")
-    //             .then(function (dbUser) {
-    //                 // If able to successfully find and associate all Users and Notes, send them back to the client
-    //                 res.json(dbUser);
-    //             })
-    //             .catch(function (err) {
-    //                 // If an error occurs, send it back to the client
-    //                 res.json(err);
-    //             });
-    //     });
-    //     app.get("/codeToday", function (req, res) {
-    //         var lastUpdateTime = new Date().getTime();
+    app.get("/populateduser", function (req, res) {
+        // Find all users
+        Event.find({})
+            // Specify that we want to populate the retrieved users with any associated notes
+            .populate("users")
+            .then(function (dbUser) {
+                // If able to successfully find and associate all Users and Notes, send them back to the client
+                res.json(dbUser);
+            })
+            .catch(function (err) {
+                // If an error occurs, send it back to the client
+                res.json(err);
+            });
+    })
+}
 
-
-    //         var currentTime = new Date().getTime();
-    //         if (currentTime - lastUpdateTime >= 24 * 60 * 60 * 1000) // number of milliseconds in a day
-    //         {
-    //             // update cycleDay
-    //             lastUpdateTime = currentTime;
-    //             // ...
-    //             code = Math.floor(100000 + Math.random() * 900000)
-    //         }
-
-    //         res.json(code)
-    //     })
-};
